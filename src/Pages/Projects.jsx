4f671ffa7426f9da6958f479c8900c52aab37b58
eborweed <ibrahimwaheed8@gmail.com>
2024-08-15 09:01:39 +1200

    centered project divs

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,25 +1,47 @@
-import React, { useContext } from "react";
-import {ThemeContext} from "../App";
+import React, { useContext, useState } from "react";
+import { ThemeContext } from "../App";
 import "./Projects.css";
-import { useState } from "react";
-const Projects=() => {
-    const [theme,changeTheme]  = useContext(ThemeContext);
-  theme=="dark"?document.body.style.setProperty("background", "#222", "important"):document.body.style.setProperty("background", "#eee", "important");
-  const [slide,setSlide]=useState(1);
+import { motion } from "framer-motion";
+import projects from "../projectinfo.jsx";
+
+const Projects = () => {
+  const [theme] = useContext(ThemeContext);
+  theme === "dark"
+    ? document.body.style.setProperty("background", "#222", "important")
+    : document.body.style.setProperty("background", "#eee", "important");
+
+  const [slide, setSlide] = useState(projects[0]);
+  const [isVisible, setIsVisible] = useState(true);
+
   function changeSlide() {
-    setSlide(slide === 1 ? 2 : 1);
+    setIsVisible(false); // Trigger fade out
+    setTimeout(() => {
+      setSlide(slide === projects[0] ? projects[1] : projects[0]);
+      setIsVisible(true); // Trigger fade in
+    }, 500); // Adjust this timeout to match the duration of the fade-out animation
   }
-  
+
   return (
-    <div className="Projects">
-      
-      <img
-      className="slide"
-        src={"./assets/Project"+slide+".png"}
-        alt="Literally Me"
-      />
-      <button className="learn-more" onClick={changeSlide}>HAHa</button>
-    </div>
+    <>
+      <div className="Text--Container">
+        <div className="Projects">
+          <motion.img
+            className="slide"
+            src={slide.imageSource}
+            alt={slide.description}
+            animate={{ opacity: isVisible ? 1 : 0 }}
+            transition={{ duration: 0.5 }}
+          />
+          <div className="buttons">
+            <button className="learn-more" onClick={changeSlide}>HAHa</button>
+            <button className="learn-more" onClick={changeSlide}>HAHa</button>
+          </div>
+        </div>
+        <motion.p animate={{ opacity: isVisible ? 1 : 0 }}
+            transition={{ duration: 0.5 }} className="text">{slide.description}</motion.p>
+      </div>
+    </>
   );
 }
+
 export default Projects;
